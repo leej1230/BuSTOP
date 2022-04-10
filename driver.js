e1caabcb1f6d8b1d34f1b16c6f9a37fe8b57c1b0
leej1230 <leejaewoo1230@gmail.com>
2022-04-09 19:16:35 -0700

    final final commit

--- driver.js
+++ driver.js
@@ -4,7 +4,6 @@ const current_sec = document.getElementById('ws');
 const currentMinute = document.getElementById('new_min');
 const currentSecond = document.getElementById('new_sec');
 
-// 現在の値を埋め込む関数
 const setCurrentValue_min = (val) => {
     currentMinute.innerText = val;
 }
@@ -13,7 +12,6 @@ const setCurrentValue_sec = (val) => {
     currentSecond.innerText = val;
 }
 
-// inputイベント時に値をセットする関数
 const rangeOnChange_min = (e) =>{
     setCurrentValue_min(e.target.value);
     input_min = e.target.value;
@@ -24,10 +22,8 @@ const rangeOnChange_sec = (e) =>{
 }
 
 window.addEventListener('load', function() {
-    // 変更に合わせてイベントを発火する
     current_min.addEventListener('input', rangeOnChange_min);
     current_sec.addEventListener('input', rangeOnChange_sec);
-    // ページ読み込み時の値をセット
     setCurrentValue(current_min.value);
     setCurrentValue(current_sec.value);
 })
